49d72f7aa4019eac824fab6b6ff0aef053c30ed4
Ferdi Pienaar <ferdi.pienaar@hotmail.com>
2026-09-19 06:37:46 +0800

    Imported event_recorder's API changed: namespace and matcher function.

--- composition_root.cpp
+++ composition_root.cpp
@@ -1,11 +1,11 @@
 /*
- * 
+ *
  */
 #include "record_table.h"
 #include "record_table_manager.h"
 #include <iostream>
 
-void dump_table_state_cb(const Record_table_op_itf & t)
+void dump_table_state_cb(const Event_record::Table_op_itf &t)
 {
     std::cout << "size " << t.size() << std::endl;
     std::cout << "enabled " << t.enabled() << std::endl;
@@ -14,12 +14,30 @@ void dump_table_state_cb(const Record_table_op_itf & t)
     std::cout << "stopped " << t.is_stopped() << std::endl;
 }
 
-int main(int argc, char * argv[])
+void dump_entry(const int &entry)
 {
-    Record_table<int> itable(Record_table_init_config().size(2).enable(), nullptr, dump_table_state_cb);
-    Record_table_manager table_mgr({{"int_table", itable}});
+    std::cout << entry << std::endl;
+}
+
+void operator_out(const std::string &msg)
+{
+    std::cout << msg;
+}
+
+int main(int argc, char *argv[])
+{
+    Event_record::Table<int> itable(Event_record::Table_init_config().size(2).enable(), dump_entry,
+                                    dump_table_state_cb);
+    Event_record::Table_manager table_mgr({{"int_table", itable}}, operator_out);
 
     itable.get_write_entry() = 1;
-    table_mgr.dump_tables_state("int_table");
+    itable.get_write_entry() = 121;
+
+    // Lambda returns true always, i.e. apply command to all tables.
+    auto matcher = [](const std::string &name) { return true; };
+
+    table_mgr.enable_tables(matcher, false);
+    table_mgr.dump_tables_state(matcher);
+    table_mgr.dump_tables(matcher);
     return 0;
 }
